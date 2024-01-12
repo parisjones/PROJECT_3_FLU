db1a9a77ef75bfa67a05c63d55fda3b1c7871bc3
parisjones <paris.jones901@gmail.com>
2024-01-11 20:41:54 -0700

    graphs for project 3

---
+++ flu_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE flu_data (
+    region_type VARCHAR(50),
+    region VARCHAR(50),
+    year INT,
+    week INT,
+    unweighted_ili DECIMAL(10, 5),
+    age_0_4 INT,
+    age_25_49 INT,
+    age_25_64 INT,
+    age_5_24 INT,
+    age_50_64 INT,
+    age_65 INT,
+    ilitotal INT,
+    num_of_providers INT,
+    total_patients INT
+);
+
+
+
